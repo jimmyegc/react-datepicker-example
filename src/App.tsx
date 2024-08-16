fbe1c76b047a943761ea7713d53e34db65e81d46
Jimmy García <jimmyegc@gmail.com>
2024-08-15 20:49:05 -0600

    fix 08152024

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,53 @@ import { WFCFormInputDatePicker } from "./WFCFormInputDatePicker/WFCFormInputDat
 import { componentsMock } from "./WFCFormInputDatePicker/componentsMock";
 import { WFCDatePicker } from "./views/WFCDatePicker/WFCDatePicker"; */
 
+import DatePicker from "react-datepicker";
 import { WFCDatePicker } from "./views/WFCDatePicker/WFCDatePicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useState } from "react";
+import { setHours, setMinutes } from "date-fns";
+
+
 
 export const App = () => {
   //const [listDisabledDays, setListDisabledDays] = useState([]);
   //const handleDisabledDaysChange = (e) => setListDisabledDays(e);
 
+  const parseRejectionReasons = (input) => {
+    return input.split('\n').map(line => {
+      const [id, reason] = line.split(',');
+      return { id: parseInt(id.trim()), reason: reason.trim() };
+    });
+  }
+
+  const input = "1,Motivo de rechazo\n2,Opcion 2\n3,No le interesa";
+  const result = parseRejectionReasons(input);
+  console.log(result);
+
+  const [startDate, setStartDate] = useState(new Date());
+  const [timeIntervals, setTimeIntervals] = useState(15)
+
   return (
     <>
-      <h1>Webform</h1>
+      <h4>Webform</h4>
       <div className="container">
-        <WFCDatePicker />
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+          locale="es"
+          showTimeSelect
+          timeFormat="p"
+          timeIntervals={timeIntervals}
+          dateFormat="Pp"
+          minTime={setHours(setMinutes(new Date(), 0), 17)}
+          maxTime={setHours(setMinutes(new Date(), 30), 20)}
+          dateFormat="MMMM d, yyyy h:mm aa"
+        />
       </div>
+
+      {/*  <div className="container">
+        <WFCDatePicker />
+      </div> */}
       {/*  <WFCFormInputDatePicker
       onChangeProps={{}}
       objConf={componentsMock.objConf}
